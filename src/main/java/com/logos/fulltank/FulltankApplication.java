89039bd629b4ba3e4d976cb7fe688166796b14bd
Nazar Tsykhuliak <nazartsykhuliak@gmail.com>
2022-08-17 15:16:52 +0300

    Init commit

---
+++ src/main/java/com/logos/fulltank/FulltankApplication.java
@@ -0,0 +1,13 @@
+package com.logos.fulltank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulltankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulltankApplication.class, args);
+	}
+
+}
